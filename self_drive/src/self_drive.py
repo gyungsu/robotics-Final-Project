265602147f50fdc320d871637cfd03d776d44da1
gyungsu <kew27@naver.com>
2020-11-29 22:00:07 +0900

    write file

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -19,10 +19,10 @@ class SelfDrive:
             turtle_vel.angular.z = 0.0
         elif scan.ranges[30] < 0.25:
             turtle_vel.linear.x = 0.0  
-            turtle_vel.angular.z = -2
+            turtle_vel.angular.z = -0.5
         elif scan.ranges[330] < 0.25:
             turtle_vel.linear.x = 0.0  
-            turtle_vel.angular.z = 2
+            turtle_vel.angular.z = 0.5
        
         else :
            turtle_vel.linear.x = 0.05  
