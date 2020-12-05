22ac57f61234ad91cb8c37176725196a4b19355f
gyungsu <kew27@naver.com>
2020-12-05 06:30:02 -1000

    final code

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -14,9 +14,9 @@ class SelfDrive:
        
         if scan.ranges[0] < 0.3:
             if (scan.ranges[45] - scan.ranges[315])>0:
-                turtle_vel.angular.z = 2.0
+                turtle_vel.angular.z = 1.8
             elif (scan.ranges[45] - scan.ranges[315])<0:
-                turtle_vel.angular.z = -2.0                
+                turtle_vel.angular.z = -1.8                
         else :
             turtle_vel.linear.x = 0.15  
         self.publisher.publish(turtle_vel)
